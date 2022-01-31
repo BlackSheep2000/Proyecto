ebf28c730be8cb46cdc86e996f36697f87dcb666
Pedro Zuñiga <56794823+PedroCandelria@users.noreply.github.com>
2022-01-30 20:45:58 -0600

    Create mapa.js

---
+++ mapa.js
@@ -0,0 +1,20 @@
+const data = {
+  0: {
+    nombre: 'Prueba',
+    area: 'test',
+    min: {
+      ram: 4,
+      cpu: 'i3',
+      gpu: 'None',
+    },
+  },
+  1: {
+    nombre: 'Minecraft',
+    area: 'juegos',
+    min: {
+      ram: 1,
+      cpu: 'i5',
+      gpu: 'CPU integrados',
+    },
+  },
+};
